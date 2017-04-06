325f9f283c500650fa974a656e8e1e73c559a702
Miiller <miilleramaral@gmail.com>
2017-04-05 22:07:55 -0300

    Aula 02

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>LaraMusic</title>
+
+    <!-- Boostrap CSS -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/bootstrap-theme.min.css">
+
+    <!-- Fonts Icons CSS -->
+    <link rel="stylesheet" href="./css/font-awesome.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/laramusic.css">
+</head>
+<body>
+    <h1 class="oculta">LaraMusic</h1>
+
+    <div class="container">
+        <header class="header">
+            <img src="imgs/laramusic.png" alt="LaraMusic" title="LaraMusic" class="img-logo">
+            <form action="" class="form-search">
+                <input type="text" name="pesquisa" placeholder="Pesquisar Música">
+            </form>
+
+            <a href="" class="login">Entrar</a>
+        </header>
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery-3.2.0.js"></script>
+
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
